fe41c80808823db902324de1dd8ab3c01700ce1b
asendia2025 <aseng@ase-semi.com>
2026-02-06 12:44:53 +0900

    Update sales_representatives_db.js

--- costing/sales_representatives_db.js
+++ costing/sales_representatives_db.js
@@ -8,7 +8,7 @@ const SALES_REPS_DB = [
         id: "dypark",
         name: "박다연",
         position: "대리",
-        dept: "영업팀",
+        dept: "Sales Team",
         email: "dypark@ase-semi.com",
         tel: "031-329-0061",
         hp: "010-7274-2617",
@@ -18,7 +18,7 @@ const SALES_REPS_DB = [
         id: "yjhan",
         name: "한예진",
         position: "사원",
-        dept: "영업팀",
+        dept: "Sales Team",
         email: "yjhan@ase-semi.com",
         tel: "031-329-0022",
         hp: "010-9314-4181",
@@ -28,7 +28,7 @@ const SALES_REPS_DB = [
         id: "sjlee",
         name: "이수정",
         position: "사원",
-        dept: "영업팀",
+        dept: "Sales Team",
         email: "soojung.lee@ase-semi.com",
         tel: "031-329-0061",
         hp: "010-4179-9597",
@@ -38,7 +38,7 @@ const SALES_REPS_DB = [
         id: "jycho",
         name: "조재영",
         position: "대리",
-        dept: "영업팀",
+        dept: "Sales Team",
         email: "jycho@ase-semi.com",
         tel: "031-329-0000", // 대표 번호 (Direct Missing)
         hp: "010-4968-3178",
@@ -48,7 +48,7 @@ const SALES_REPS_DB = [
         id: "jwlee",
         name: "이종욱",
         position: "차장",
-        dept: "영업팀",
+        dept: "Sales Team",
         email: "jwlee@ase-semi.com",
         tel: "031-329-0000", // 대표 번호 (Direct Missing)
         hp: "010-8314-4138",
@@ -58,7 +58,7 @@ const SALES_REPS_DB = [
         id: "hdshin",
         name: "신현덕",
         position: "이사",
-        dept: "영업본부",
+        dept: "Sales Team",
         email: "hdshin@ase-semi.com",
         tel: "031-329-0000", // 대표 번호 (Direct Missing)
         hp: "010-9848-2330",
@@ -68,7 +68,78 @@ const SALES_REPS_DB = [
         id: "tjben",
         name: "변태준",
         position: "상무",
-        dept: "영업본부",
+        dept: "Sales Team",
+        email: "tjben@ase-semi.com",
+        tel: "031-329-0000", // 대표 번호 (Direct Missing)
+        hp: "010-2376-3891",
+        fax: "031-329-0001"
+    },
+
+    {
+        id: "dypark",
+        name: "Dayeon Park",
+        position: "",
+        dept: "Sales Team",
+        email: "dypark@ase-semi.com",
+        tel: "031-329-0061",
+        hp: "010-7274-2617",
+        fax: "031-329-0001"
+    },
+    {
+        id: "yjhan",
+        name: "Yejin Han",
+        position: "",
+        dept: "Sales Team",
+        email: "yjhan@ase-semi.com",
+        tel: "031-329-0022",
+        hp: "010-9314-4181",
+        fax: "031-329-0001"
+    },
+    {
+        id: "sjlee",
+        name: "Sujung Lee",
+        position: "",
+        dept: "Sales Team",
+        email: "soojung.lee@ase-semi.com",
+        tel: "031-329-0061",
+        hp: "010-4179-9597",
+        fax: "031-329-0001"
+    },
+    {
+        id: "jycho",
+        name: "Jonathan",
+        position: "",
+        dept: "Sales Team",
+        email: "jycho@ase-semi.com",
+        tel: "031-329-0000", // 대표 번호 (Direct Missing)
+        hp: "010-4968-3178",
+        fax: "031-329-0001"
+    },
+    {
+        id: "jwlee",
+        name: "John Lee",
+        position: "",
+        dept: "Sales Team",
+        email: "jwlee@ase-semi.com",
+        tel: "031-329-0000", // 대표 번호 (Direct Missing)
+        hp: "010-8314-4138",
+        fax: "031-329-0001"
+    },
+    {
+        id: "hdshin",
+        name: "HD Shin",
+        position: "",
+        dept: "Sales Team",
+        email: "hdshin@ase-semi.com",
+        tel: "031-329-0000", // 대표 번호 (Direct Missing)
+        hp: "010-9848-2330",
+        fax: "031-329-0001"
+    },
+    {
+        id: "tjben",
+        name: "TJ Ben",
+        position: "",
+        dept: "Sales Team",
         email: "tjben@ase-semi.com",
         tel: "031-329-0000", // 대표 번호 (Direct Missing)
         hp: "010-2376-3891",
